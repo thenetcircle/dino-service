b6cedc9dd53e6636559a75a9669ee86d05323442
Oscar Eriksson <oscar@thenetcircle.com>
2020-06-03 16:55:57 +0800

    mock threads in login response

--- dinofw/thread/handler.py
+++ dinofw/thread/handler.py
@@ -3,4 +3,37 @@ class ThreadHandler:
         self.env = gn_env
 
     def threads_for(self, user_id: str):
-        return list()
+        return [
+            {
+                "id": 0,
+                "preview": "some group with baz",
+                "last_user": "normaloaccount",
+                "users_list": [{
+                    "id": 1234,
+                    "name": "foo",
+                    "avatar": "https://example.com/image/1234.jpg"
+                }, {
+                    "id": 4321,
+                    "name": "bar",
+                    "avatar": "https://example.com/image/4321.jpg"
+                }, {
+                    "id": 5678,
+                    "name": "baz",
+                    "avatar": "https://example.com/image/5678.jpg"
+                }]
+            },
+            {
+                "id": 1,
+                "preview": "bar",
+                "last_user": "foo",
+                "users_list": [{
+                    "id": 1234,
+                    "name": "foo",
+                    "avatar": "https://example.com/image/1234.jpg"
+                }, {
+                    "id": 4321,
+                    "name": "bar",
+                    "avatar": "https://example.com/image/4321.jpg"
+                }]
+            }
+        ]
