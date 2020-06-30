53245bca63878c3cfa59d2518942523561fca73c
Oscar Eriksson <oscar@thenetcircle.com>
2020-06-30 14:29:14 +0800

    better api method naming

--- dinofw/restful.py
+++ dinofw/restful.py
@@ -35,22 +35,22 @@ async def group_messages_for_user(group_id: str, user_id: int, query: HistoryQue
 
 
 @app.get("/v1/groups/{group_id}/users/{user_id}/messages/{message_id}", response_model=Message)
-async def group_get_message(group_id: str, user_id: int, message_id: str) -> Message:
+async def get_message_details(group_id: str, user_id: int, message_id: str) -> Message:
     return await environ.env.rest.group.message(group_id, user_id, message_id)
 
 
 @app.post("/v1/groups/{group_id}/users", response_model=GroupUsers)
-async def group_users(group_id: str, query: PaginationQuery) -> GroupUsers:
+async def users_in_group(group_id: str, query: PaginationQuery) -> GroupUsers:
     return await environ.env.rest.user.users(group_id, query)
 
 
 @app.get("/v1/userstats/{user_id}", response_model=UserStats)
-async def user_stats(user_id: int) -> UserStats:
+async def user_statistics(user_id: int) -> UserStats:
     return await environ.env.rest.user.stats(user_id)
 
 
 @app.get("/v1/groups/{group_id}/userstats/{user_id}", response_model=UserGroupStats)
-async def user_group_stats(group_id: str, user_id: int) -> UserGroupStats:
+async def user_statistics_in_group(group_id: str, user_id: int) -> UserGroupStats:
     return await environ.env.rest.group.stats(group_id, user_id)
 
 
@@ -60,7 +60,7 @@ async def search_groups(query: SearchQuery) -> List[Group]:
 
 
 @app.get("/v1/groups/{group_id}", response_model=Group)
-async def get_group(group_id) -> Group:
+async def group_information(group_id) -> Group:
     return await environ.env.rest.group.get_group(group_id)
 
 
