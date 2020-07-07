8f86e6f40b87abd0073cab20bcda7f92c5392497
Oscar Eriksson <oscar@thenetcircle.com>
2020-07-07 10:32:42 +0800

    messages on top

--- dinofw/db/cassandra/models.py
+++ dinofw/db/cassandra/models.py
@@ -7,6 +7,38 @@ from cassandra.cqlengine.columns import Text
 from cassandra.cqlengine.models import Model
 
 
+class MessageModel(Model):
+    __table_name__ = "messages"
+
+    group_id = UUID(
+        required=True,
+        primary_key=True,
+        partition_key=True,
+    )
+    created_at = DateTime(
+        required=True,
+        primary_key=True,
+        clustering_order="DESC",
+    )
+    user_id = Integer(
+        required=True,
+        primary_key=True,
+    )
+    message_id = UUID(
+        required=True,
+        default=uuid.uuid4
+    )
+    message_payload = Text(
+        required=True
+    )
+
+    status = Integer()
+    message_type = Integer()
+    updated_at = DateTime()
+    removed_by_user = Integer()
+    last_action_log_id = UUID()
+
+
 class JoinerModel(Model):
     __table_name__ = "joiners"
 
@@ -63,35 +95,3 @@ class ActionLogModel(Model):
 
     admin_id = Integer()
     message_id = UUID()
-
-
-class MessageModel(Model):
-    __table_name__ = "messages"
-
-    group_id = UUID(
-        required=True,
-        primary_key=True,
-        partition_key=True,
-    )
-    created_at = DateTime(
-        required=True,
-        primary_key=True,
-        clustering_order="DESC",
-    )
-    user_id = Integer(
-        required=True,
-        primary_key=True,
-    )
-    message_id = UUID(
-        required=True,
-        default=uuid.uuid4
-    )
-    message_payload = Text(
-        required=True
-    )
-
-    status = Integer()
-    message_type = Integer()
-    updated_at = DateTime()
-    removed_by_user = Integer()
-    last_action_log_id = UUID()
