0382db48320d10717885185aed758ad2be92a791
Oscar Eriksson <oscar@thenetcircle.com>
2021-10-21 16:00:48 +0800

    from method name

--- dinofw/rest/broadcast.py
+++ dinofw/rest/broadcast.py
@@ -4,7 +4,7 @@ from dinofw.rest.base import BaseResource
 
 
 class BroadcastResource(BaseResource):
-    async def send_message_to_group(
+    async def broadcast_event(
         self, group_id: str, event: dict, db: Session
     ) -> None:
         _, users_and_join_time, _ = self.env.db.get_users_in_group(group_id, db, include_group=False)
