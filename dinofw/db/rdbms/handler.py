dea2d4b8f8f8b741fb846e980979f0e2a7555a2b
Oscar Eriksson <oscar@thenetcircle.com>
2022-06-17 14:26:14 +0800

    clear unread, don't set to 0

--- dinofw/db/rdbms/handler.py
+++ dinofw/db/rdbms/handler.py
@@ -1150,7 +1150,7 @@ class RelationalHandler:
             user_stats.last_read = last_read
 
             # recount unread from cassandra and save in cache and db
-            self.env.cache.reset_unread_in_groups(user_id, [group_id])
+            self.env.cache.clear_unread_in_group_for_user(group_id, user_id)
             user_stats.unread_count = self.env.storage.get_unread_in_group(group_id, user_id, last_read)
 
             # highlight time is removed if a user reads a conversation
