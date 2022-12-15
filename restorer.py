f58026d6a5bb45ca6a02bf1520a5b0ec3cc4c103
Oscar Eriksson <oscar.eriks@gmail.com>
2022-12-15 12:46:15 +0100

    app file for restorer

---
+++ restorer.py
@@ -0,0 +1,29 @@
+import logging
+import os
+
+from dinofw.utils.config import ConfigKeys
+
+logging.basicConfig(
+    format=ConfigKeys.DEFAULT_LOG_FORMAT,
+    datefmt=ConfigKeys.DEFAULT_DATE_FORMAT,
+    level=logging.DEBUG
+)
+
+logging.getLogger("cassandra").setLevel(logging.INFO)
+logging.getLogger("gmqtt").setLevel(logging.WARNING)
+logging.getLogger("kafka").setLevel(logging.INFO)
+
+CQL_ALLOW_MNG = "CQLENG_ALLOW_SCHEMA_MANAGEMENT"
+DELETER_KEY = "DINO_DELETER"
+
+# don't allow deleter to change schema
+os.environ[CQL_ALLOW_MNG] = "0"
+
+# indicate this is the restorer service (won't initialize unnecessary modules)
+os.environ[DELETER_KEY] = "1"
+
+from dinofw.utils import environ
+environ.env.node = "restorer"
+
+# keep this import; even though unused, uvicorn needs it, otherwise it will not start
+from dinofw.restore import app  # noqa
