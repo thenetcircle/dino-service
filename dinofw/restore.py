9494aa63cf263cb9477d75abad991ae8f9fbf1c3
Oscar Eriksson <oscar.eriks@gmail.com>
2022-12-15 12:53:36 +0100

    more logging

--- dinofw/restore.py
+++ dinofw/restore.py
@@ -24,6 +24,8 @@ class Restorer:
             return
 
         groups_to_fix, existing_user_ids = self.get_groups_and_users_to_fix(groups, session)
+        logger.info(f"existing_user_ids: {existing_user_ids}")
+        logger.info(f"groups_to_fix: {[group.group_id for group in groups_to_fix]}")
         if not len(existing_user_ids):
             logger.info("no existing users left to fix")
             return
@@ -36,7 +38,7 @@ class Restorer:
         logger.info(f"about to create {len(stats_to_create)} stats")
         self.env.db.create_stats_for(stats_to_create, session)
 
-    def get_groups_and_users_to_fix(self, groups: List[GroupBase], session) -> (List[GroupBase], List[int]):
+    def  get_groups_and_users_to_fix(self, groups: List[GroupBase], session) -> (List[GroupBase], List[int]):
         unique_user_ids = set()
         groups_to_fix = list()
 
@@ -79,6 +81,8 @@ class Restorer:
                     user_to_fix = user
                     break
 
+            logger.info(f"user_ids: {user_ids}")
+            logger.info(f"user_to_fix: {user_to_fix}")
             if user_to_fix is None:
                 logger.info(f"not fixing group {group.group_id} ({group.name}), both users deleted their profile")
                 continue
