1988d2e12eaaecb8a240a6bc2cf9a2a3e38aa6d3
Oscar Eriksson <oscar@thenetcircle.com>
2023-03-20 13:13:19 +0800

    db migration script for kicked column

---
+++ alembic/versions/25caf1d2067e_added_kicked_column.py
@@ -0,0 +1,31 @@
+"""added kicked column
+
+Revision ID: 25caf1d2067e
+Revises: a4026fb9cf4c
+Create Date: 2023-03-20 05:12:32.505097+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import Column, Boolean
+
+
+# revision identifiers, used by Alembic.
+revision = '25caf1d2067e'
+down_revision = 'a4026fb9cf4c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        table_name="user_group_stats",
+        column=Column("kicked", Boolean(), nullable=False, server_default="true", default=True)
+    )
+
+
+def downgrade():
+    op.drop_column(
+        table_name="user_group_stats",
+        column_name="kicked"
+    )
