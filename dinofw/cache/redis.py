4c0e83a4ceaccf32ed2bff308a8d197c447bd935
Oscar Eriksson <oscar@thenetcircle.com>
2023-03-24 09:39:22 +0800

    max client id

--- dinofw/cache/redis.py
+++ dinofw/cache/redis.py
@@ -85,6 +85,9 @@ class CacheRedis(ICache):
             self.listen_port = args[bind_arg_pos + 1].split(":")[1]
 
         self.listen_host = socket.gethostname().split(".")[0]
+        self.max_client_ids = int(float(
+            env.config.get(ConfigKeys.MAX_CLIENT_IDS, domain=ConfigKeys.CACHE_SERVICE, default=10)
+        ))
 
     @contextmanager
     def pipeline(self):
