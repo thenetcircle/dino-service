47ae431c2e05c806756d1093afe858c3334be731
Oscar Eriksson <oscar.eriks@gmail.com>
2023-07-11 13:55:13 +0800

    fixed test case

--- test/functional/test_group_count_cache.py
+++ test/functional/test_group_count_cache.py
@@ -13,11 +13,11 @@ class TestGroupCountCache(BaseServerRestApi):
         )
         self.get_global_user_stats(BaseTest.USER_ID, hidden=False, count_unread=True)
         the_count = self.env.cache.get_count_group_types_for_user(user_id=BaseTest.USER_ID, hidden=False)
-        self.assertEqual([(1, 1)], the_count)
+        self.assertEqual([(1, 1), (0, 0)], the_count)
 
         self.send_1v1_message(
             user_id=BaseTest.USER_ID,
             receiver_id=BaseTest.THIRD_USER_ID
         )
         the_count = self.env.cache.get_count_group_types_for_user(user_id=BaseTest.USER_ID, hidden=False)
-        self.assertEqual([(1, 2)], the_count)
+        self.assertEqual([(1, 2), (0, 0)], the_count)
