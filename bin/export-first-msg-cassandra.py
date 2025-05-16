44918579103f4e3ecf3de66128173c1af90c3d7f
Oscar Eriksson <oscar@thenetcircle.com>
2025-05-16 12:45:39 +0800

    use dotenv and cassandra driver, not shell command

--- bin/export-first-msg-cassandra.py
+++ bin/export-first-msg-cassandra.py
@@ -1,8 +1,14 @@
-import subprocess
-import sys
-import arrow
-from tqdm import tqdm
 import os
+import sys
+
+import arrow
+import dotenv
+from cassandra.cluster import Cluster
+from cassandra.cluster import PlainTextAuthProvider
+from cassandra.cqlengine import connection
+from tqdm import tqdm
+
+dotenv.load_dotenv()
 
 
 def group_id_to_users(group_id_: str) -> (int, int):
@@ -29,16 +35,16 @@ def split_into_chunks(objects, n):
 """
 get group ids using this query:
 
-dinoms_feti_prod=# copy (select group_id from groups where updated_at >= '2025-04-24 06:40:00+00' and first_message_time < '2025-04-24 07:00:00+00') to '/tmp/popp_msgs_psql-250506.csv' (format csv);
+dinoms_feti_prod=# copy (select group_id from groups where updated_at >= '2025-05-06 20:43:13+00' and first_message_time <= '2025-05-06 20:45:38+00') to '/tmp/feti_msgs_psql-250516-2.csv' (format csv);
 COPY 108091
 """
 
 with open(sys.argv[1]) as f:
     group_ids = [g.replace('\n', '') for g in f.readlines()]
 
-from_time = '2025-04-24 06:40:00.000+0000'
-to_time = '2025-04-24 07:00:00.000+0000'
-outname = 'popp_msgs_cassandra-250506.csv'
+from_time = '2025-05-06 16:19:00.000+0000'
+to_time = '2025-05-06 16:21:03.000+0000'
+outname = 'feti_msgs_cassandra-250516.csv'
 existing = set()
 batch_size = 75
 
@@ -48,6 +54,26 @@ if os.path.exists(outname):
             existing.add(','.join(sorted(line.split(',')[0:2])))
 
 print(f'found {len(existing)} existing exports already')
+
+kwargs = {
+    "default_keyspace": os.environ['CASD_KEY_SPACE'],
+    "protocol_version": 3,
+    "retry_connect": True,
+    "auth_provider": PlainTextAuthProvider(
+        username=os.environ['CASD_USER'],
+        password=os.environ['CASD_PASS']
+    )
+}
+connection.setup(os.environ['CASD_HOSTS'].split(','), **kwargs)
+
+cluster = Cluster(
+    contact_points=os.environ['CASD_HOSTS'],
+    protocol_version=3,
+    auth_provider=kwargs["auth_provider"]
+)
+
+session = cluster.connect(os.environ['CASD_KEY_SPACE'])
+
 
 with open(outname, 'wa') as f:
     if not len(existing):
@@ -63,41 +89,19 @@ with open(outname, 'wa') as f:
         if not len(group_ids):
             continue
 
-        args = [
-            "/usr/local/tncdata/apps/apache-cassandra-3.11.8/bin/cqlsh",
-            "--request-timeout=3600",
-            "casd1",
-            "-k",
-            "dinoms_feti_prod",
-            "-e"
-            "paging off; select group_id, created_at, user_id, message_type from messages where group_id in ({}) and created_at > '{}' and created_at < '{}'".format(
+        try:
+            rows = session.execute("select group_id, created_at, user_id, message_type from messages where group_id in ({}) and created_at > '{}' and created_at < '{}'".format(
                 ','.join(group_ids),
                 from_time,
                 to_time
-            )
-        ]
+            ))
 
-        cmd_output = str(subprocess.check_output(args), 'utf-8')
+            for row in rows:
+                receiver_id = receiver_for(row.group_id, row.user_id)
 
-        try:
-            for line in cmd_output.split('\n')[3:-3]:
-                if line.startswith('----') or not len(line.strip()):
-                    continue
-
-                try:
-                    group_id, created_at, sender_id, message_type = map(str.strip, line.split('|'))
-                except Exception as e2:
-                    print(e2)
-                    print(line)
-                    print(cmd_output)
-                    continue
-
-                receiver_id = receiver_for(group_id, sender_id)
-
-                line_to_write = ",".join([sender_id, receiver_id, message_type, str(arrow.get(created_at).float_timestamp)])
+                line_to_write = ",".join([row.user_id, receiver_id, row.message_type, str(arrow.get(row.created_at).float_timestamp)])
                 f.write(line_to_write + '\n')
 
         except Exception as e:
             print(e)
-            print(cmd_output)
             raise e
